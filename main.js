f433e76416f0e177dd096e4e118caee59e8a217d
ZaroSan <martin2609393@gmail.com>
2022-03-24 20:48:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+import BuilderFaker from "./javascript/builder.js";
 import { faker } from '@faker-js/faker';
+
+const name=faker.word.random();
+
+const builder=new BuilderFaker(["id","name","email"]);
